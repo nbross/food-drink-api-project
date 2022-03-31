3a42ad515130892598132e980606eca6aa7c5abf
nbross <nbross2001@gmail.com>
2022-03-30 20:58:50 -0400

    added the api links to one of the js files

--- assets/js/meal.js
+++ assets/js/meal.js
@@ -1,1 +1,3 @@
 // this for the input and what we can put in that text bar to give us a output
+// www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita - by name
+// www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin - by ingredient 
--- assets/js/wine.js
+++ assets/js/wine.js
@@ -1,1 +1,2 @@
 // this is the page that will give us that output and dictate what it gives and how it looks
+// 
