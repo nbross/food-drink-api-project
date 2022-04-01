e883d1dc5c7c79123978ec490ddae080565b192d
KyleJeffreyGeary <gearykyle24@gmail.com>
2022-04-01 17:49:19 -0400

    Merge branch 'develop' of github.com:nbross/food-drink-api-project into develop

--- assets/js/search_history.js
+++ assets/js/search_history.js
@@ -1,1 +1,7 @@
 // this will be responsible for generating wha comes out of the search history section
+const displaySearchHistory = (newSearchTerm) = {
+    let searchHistory = {};
+    if (localStorage.getItem('search_history')) {
+        searchHistory = JSON.parse(localStorage.getItem('search_history'));
+    };
+}
