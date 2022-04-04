d1b0dcc1e31ebebcaded0f19cd0f6b95572c0cd3
KyleJeffreyGeary <gearykyle24@gmail.com>
2022-04-04 19:04:11 -0400

    Enter

--- assets/js/search_history.js
+++ assets/js/search_history.js
@@ -6,15 +6,15 @@ const displaySearchHistory = (newSearchTerm) => {
         searchHistory = JSON.parse(localStorage.getItem('search_history'));
     };
 }
-/*Check if search history includes this term*/
+//Check if search history includes this term//
 if (newSearchTerm in searchHistory) {}
 else if (newSearchTerm && newSearchTerm.length > 0) {
-    /*If there is a new term it will be added to search history*/
+    //If there is a new term it will be added to search history//
     searchHistory[newSearchTerm] = {text: newSearchTerm};
     localStorage.setItem('search_history', JSON.stringify(searchHistory));
 };
 
-/*if the search history is not empty, it shows on the page*/
+//if the search history is not empty, it shows on the page//
 if (object.keys(searchHistory).length > 0){
     $('#history').addClass('hidden');
     updateSearchHistoryList(searchHistory);
