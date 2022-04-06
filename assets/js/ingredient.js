4f4a138d2ed3f508ad2ebd4f5d0a662859dcf5ef
Tanya Jurecki <tljurecki@gmail.com>
2022-04-06 19:14:17 -0400

    updated html and js

--- assets/js/ingredient.js
+++ assets/js/ingredient.js
@@ -1,13 +1,128 @@
-var getDrinkByIngredient = function() {
-    var apiUrl = "https://wwww.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
+var search;
+var drinksArray = [];
+var cocktailRecipeEl = document.querySelector("#recipe");
+var searchContainerEl = document.querySelector("#search-results-container");
+
+$(document).ready(function() {
+    /* grabs the search history from localStorage and displays/loads it on the first page */
+    displaySearchHistory();
+    
+    /* Event listener for submitting search form directly */
+    $('#search-form').on('submit', function(event) {
+        event.preventDefault();
+        let drinkSearchTerm = $('#search-field-2').val();
+        runSearch(drinkSearchTerm);
+    });
+
+    /* Event listener for clicking on search button */
+    $('#search-button').on('click', function(event) {
+        let drinkSearchTerm = $('#search-field-2').val();
+        runSearch(drinkSearchTerm);
+    });
+
+    /* This function is to help run the search */
+    const runSearch = (drinkSearchTerm) => {
+        getDrinkResults(drinkSearchTerm);
+    };
+});
+
+var getDrinkByIngredient = function(drinkSearchTerm) {
+    var apiUrl = "https://wwww.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkSearchTerm;
     fetch(apiUrl).then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data) {
-                getDrinkByIngredient();
-            })
-        } else {
+        var searchResultsEl = $('#search-results');
+        drinksArray = response.drinks;
+        
+        searchResultsEl.empty();
+        searchResultsContainerEl.classList = "display", "block";
+        cocktailRecipeEl.classList = "display", "none";
+     
+     if (drinksArray === null) {
             var errorMessage = $('<p>').text("No results found.  Please try another search!");
             $("#search-field").append(errorMessage);
+        } else {
+            displaySearchHistory(drinkSearchTerm);
+            var historyElement = document.querySelector('[data-search="' + drinkSearchTerm + '"]');
+
+            var drinkOptionsReturned = '${ingredient} (${drinksArray.length})';
+
+            var searchHistory = JSON.parse(localStorage.getItem('search_history'));
+            searchHistory[drinkSearchTerm].text = drinkOptionsReturned;
+
+            localStorage.setItem('search_hitsory', JSON.stringify(drinkSearchTerm));
+
+            historyElement.innerHTML = drinkOptionsReturned;
+            displayDrinkResults();
+
+    };
+});
+
+var displayDrinkResults = function() {
+    for(obj of drinksArray) {
+        var resultElement = $('<div>').attr('class', 'column is-3');
+        var resultsLink = $('<a id="' + obj.idDrink + '">');
+        var resultImg = $('<img>').attr('width', '200');
+        resultImg.attr('src', obj.strDrinkThumb);
+        var resultPara = $('<p>').text(obj.strDrink);
+
+        resultsLink.attr("onclick", "drinkRecipeSelected(event");
+        resultsLink.append(resultImg);
+        resultsLink.append(resultPara);
+        resultElement.append(resultsLink);
+
+        $('#search-results').append(resultElement);
+        drinkRecipeSelected();
+
+    }
+
+    var drinkRecipeSelected = function(event) { 
+        if(event.target.localName === "img" || event.target.localName === "p") {
+            drinkSelection(event.target.parentNode.id);
+        } else {
+            drinkSelection(event.target.id);
         }
-    });
-};
+    }
+
+    var drinkSelection = function(selDrinkId) {
+        var drinkSelectionArray = [];
+
+        var selDrinkObj = drinksArray.find(drinksArray => drinksArray.idDrink === selDrinkId);
+        var drinkTitleEl = $('#title');
+        var drinkImgEl = $('#recipe_img');
+        var ingredientEl = $('#ingredient');
+        var measurementEl = $('#measurement');
+        var instructionsEl = $('#instructions');
+
+        searchContainerEl.classList = 'display', 'none';
+        cocktailRecipeEl.classList = "display", "block";
+        ingredientEl.empty();
+        measurementEl.empty();
+
+        drinkTitleEl.text(selDrinkObj.strDrink);
+
+        drinkImgEl.attr("src" , selDrinkObj.strDrinkThumb);
+        instructionsEl.text(selDrinkObj.strInstructions);
+
+        for( var i = 1; i <= 20; i++) {
+            var ingredient = selDrinkObj["strIngredients" + i];
+            var measurement = selDrinkObj["strMeasure" + i];
+
+            if(ingredient !== ""  && ingredient !== null) {
+                var ingredientListItem = $('<li>');
+
+                ingredientListItem.text(ingredient);
+                ingredientEl.append(ingredientListItem);
+
+                var measurementListItem = $('<li>');
+
+                measurementListItem.text(measurement);
+                measurementEl.append(measurementListItem);
+                drinkSelectionArray.push({"ingredient": ingredient, "quantitiy": measurement});
+            } else {
+                break;
+            };
+        }
+    }
+
+
+}
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,124 @@
-// this will be used for when the page refreshes ie: what data will be loaded?
-$(document).ready(function() {
-    /* grabs the search history from localStorage and displays/loads it on the first page */
-    displaySearchHistory();
+var searchCocktail = document.getElementById('searchCocktail');
+
+searchCocktail.addEventListener('change', () => {
+    var searchValue = searchCocktail.value;
+    getCocktailByName(searchValue);
+})
+
+//function with API to return a random cocktail
+var randomCocktail = function() {
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    fetch(apiUrl).then((response) => response.json())
+    .then((cocktailInfo) => {
+        displayrandomCocktail(cocktailInfo)
+    })
+    .catch((error) => {
+        if (document.getElementById("search-cocktail").value == "") {
+            showErrMsg(error)
+        }else {
+            hideErrMsg(error)
+        };
+    })
+}
+
+//function with API to search for a cocktail by name
+
+var getCocktailByName = function (cocktail) {
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}";
     
-    /* Event listener for submitting search form directly */
-    $('#search-form').on('submit', function(event) {
-        event.preventDefault();
-        let drinkSearchTerm = $('#search-field').val();
-        runSearch(drinkSearchTerm);
-    });
+    fetch(apiUrl).then((response) => response.json())
+    .then((cocktailInfo) => {
+        displayDrinkResult(cocktailInfo)
+    })
+    .catch((error) => {
+        if (document.getElementById("search-cocktail").value == "") {
+            showErrMsg(error)
+        } else {
+            hideErrMsg(error)
+        };
+    })
+} 
 
-    /* Event listener for clicking on search button */
-    $('#search-button').on('click', function(event) {
-        let drinkSearchTerm = $('#search-field').val();
-        runSearch(drinkSearchTerm);
-    });
+//function to display random cocktail results
+var displayRandomCocktail = function (cocktailInfo) {
+    var { drinks } = cocktailInfo;
+    var cocktailImgEl = document.getElementById('cocktailPicture');
+    var cocktailPicture = ' <img class="drinkThumb" src="${drinks.[0].strDrinkThumb}">';
+    cocktailImgEl.innerHTML = cocktailPicture;
 
-    /* This function is to help run the search */
-    const runSearch = (drinkSearchTerm) => {
-        drinkSearchHandler(drinkSearchTerm);
-    };
-});
+    //display cocktail info
+    var cocktailTitleEl= document.getElementById('cocktailTitle');
+
+    var cocktailData = '<h4 class="cocktailName">${drinks[0].strDrink}</h4> <pclass+"typeOfglass">Type of glass:</p><p>${dinks[0].strGlass}</p. </br><p class="howToMake>How to make:</p>'
+    cocktailTitleEl.innerHTML = cocktailData;
+
+    var cocktailInfoEl = document.getElementById('howTo');
+
+    var result = "";
+    for(var i = 1; i <= 15; i++) {
+        var measures = 'strMeasure' + i;
+        var ingredients = 'strIngredients' + i;
+        if ((drinks[0][measures]) && (drinks[0][ingredients]) !=="") {
+            result = result = '<p>${drinks[0}{measures]} ${drinks[0][ingredients]}</p>';
+
+            cocktailInfoEl.innerHTML = result;
+        };
+    }
+
+        var cocktailInstructionsEl = document.getElementById('instructions');
+        var instructions = '</br><p>${drinks[0].strInstructions}</p>';
+        cocktailInstructionsEl.innerHTML = instructions;    
+        }
+
+        //display the drink by name results
+        var displayDrinkResult = function(cocktailInfo) {
+            var { drinks } = cocktailInfo;
+            var cocktailImgEl = document.getElementById('cocktailPicture');
+            var cocktailPicture = '<img class="drinkThumb" src="{drinks[0].strDrinkThumb}">';
+            cocktailImgEl.innerHTML = cocktailPicture;
+
+            //display cocktail info
+            var cocktailTitleEl = document.getElementById('cocktailTitle');
+
+            var cocktailData = '<h4 class="cocktailName">${drinks[0].strGlass}<p> </br><p class="howToMake">How to makes:</p>';
+            cocktailTitleEl.innerHTML = cocktailData;
+
+            var cocktailInfoEl = document.getElementById('howTo');
+
+            var result = "";
+            for(var i = 1; i <= 15; i++) {
+                var measures = 'strMeasure' + i;
+                var ingredients = 'strIngredients' + i;
+                if((drinks[0][measures]) && (drinks[0][ingredients]) !== "") {
+                    result= result + '<p>${drinks[0][measures]} ${drinks[0][ingredients]}</p>';
+
+                    cocktailInfoEl.innerHTML = result;
+                };
+            }
+
+            var cocktailInstructionsEl = document.getElementById('instructions');
+            var instructions = '</br><p>${drinks[0].strInstructions}</p>';
+             cocktailInstructionsEl.innerHTML = instructions;
+        }
+
+        //button shake
+        $(document).ready(function() {
+            $("#shakebutton").click(function(){
+                $("#shakebutton").effect("shake", { times: 3}, 450);
+            });
+        });
+
+        //Display error message for empty search
+
+        var showErrMsg = function(error) {
+            var errorEl = document.getElementById('errormsg');
+            var errorMsg = '<p class"errormsg">Please enter a drink name!</p>';
+            errorEl.innerHTML = errorMsg;
+        };
+
+        var hideErrMsg = function(error) {
+            var errorEl = document.getElementById('errormsg');
+            errorEl.innerHTML = "";
+        };
+
